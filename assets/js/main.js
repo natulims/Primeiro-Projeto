e347d896b99e6b17ac192674c0acdddd06eee462
Natalia Carvalho <ssspinosaure@gmail.com>
2022-12-12 19:20:01 -0300

    dando upload nos arquivos

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+$("body").css("backgroundColor", "#2a2928");
+
+// $("#hobbies").click(function () {
+//   $(".hobbies__info").hide();
+// });
+
+// $(".footer__link").hover(function () {
+// $(this).css("background-color", "#ff676d");
+// });
